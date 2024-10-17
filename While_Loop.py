da89bd7e892cda6b8f4adb95e01f53642c54b884
seema-maurya <123566532+seema-maurya@users.noreply.github.com>
2024-10-17 19:11:18 +0530

    Add files via upload

---
+++ While_Loop.py
@@ -0,0 +1,59 @@
+# while loops
+ind = 5
+while (ind >= 1):
+    print("Hello seema")
+    ind -=1
+
+
+
+#string
+    name = "120"
+    import sys
+    print(type(name))
+
+para = "Hello this is my first para, Welcome and have a look"
+
+
+
+
+#methods in string
+print(para.lower())
+print(para.upper())
+
+
+#accessing characters
+print(para[15])
+print(para[15:25])
+
+print(para[2:])
+
+print(para[ :40])
+
+print(para[-1:-25])
+
+
+
+# methods
+print(para + " ",  "snehal")
+
+print("seema" *3)
+
+
+#in, not in
+print("my" in para)
+
+print("seema" not in para)
+
+
+# replace method
+name = "seema"
+print(name.replace("seema","Archana"))
+
+# to remove extra spaces
+name= "         seema           "
+print(name)
+print(name.strip())
+print(name.lstrip())
+print(name.rstrip())
+
+
